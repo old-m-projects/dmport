96a0651c424f372e40b93191231035f809bfa836
gregkeys <gregkeys@gmail.com>
2016-05-26 19:04:17 -0700

    add more technical debt

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var exec = require('child_process').exec;
 var pack = require('./../package.json');
 
-describe('docker-machine import/export', function(){
+describe('dmport export', function(){
 	it('should show version', function(done){
 		exec('node index.js -V', function(error, stdout, stderr) {
 			if (error) {
@@ -25,5 +25,21 @@ describe('docker-machine import/export', function(){
 	
 	it('should throw an error if no option given');
 	it('should throw an error if the export option given is invalid');
-	it('should throw an error if the import option given is invalid');
+	it('should get the cert path for the machine given');
+	it('should get the cert & Machine storage path');
+	it('should get the env vars for the machine given');
+	it('should read the cert files and convert them to base64');
+	it('should read the machine files and convert them to base64');
+	it('should find and replace the paths in the config.json with easy to find placeholders');
+	it('should output a json string with the machine env vars as well as the file required to connect to a machine');
 });
+
+describe('dmport import', function(){
+	it('should grab the value');
+	it('should throw an error if the import option given is invalid');
+	it('should parse the env vars and export them to the shell');
+	it('should parse the import value and write the values to the proper files');
+	it('should add the correct paths to the config.json');
+});
+
+
